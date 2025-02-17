a909f286b96534704b41fce37d28dfabf92e49ae
nahollo <skgudwlsdlrj@gmail.com>
2025-02-17 09:59:11 +0900

    first commit
    first commit

---
+++ comprehensive-project/src/main/java/com/example/comprehensive/ComprehensiveProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.comprehensive;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ComprehensiveProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ComprehensiveProjectApplication.class, args);
+	}
+
+}
---
+++ comprehensive-project/src/test/java/com/example/comprehensive/ComprehensiveProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.comprehensive;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ComprehensiveProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
